90b7e2d14f49818d5748b341b3cc6b0463f00e93
QuandalePr1ngle <133515732+QuandalePr1ngle@users.noreply.github.com>
2024-03-20 10:19:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,16 +15,31 @@
 # - iespēja apskatīt vidējo izdevumu summu
 # [!] Programmai jaglabā izdevumu stāvokli kad programma ir izslēgta palaista no jauna
 #
-
 expenses = []
-
+import json
+expenses_file = open('expenses.json')
+expenses = json.load(expenses_file)
 # load expenses from expenses.json file here
 # https://www.geeksforgeeks.org/read-write-and-parse-json-using-python/ (Python read JSON file)
 pass
 
 while True:
-    command = input("\nChoose command:")
+    command = input("\nChoose command: ")
     if command == "1":
+        name = input("Enter your name: ")
+        category =input("Enter the category name: ")
+        money = input("Enter the amount of money you spent: ")
+        dictionary = {
+        "Name": name,
+        "category": category,
+        "summ": money
+        }
+        expenses.append(dictionary)
+        pass
+
+    if command == "2":
+        def showExpenses(expenses):
+            return
         pass
     if command == "e":
         print("Exiting...")
@@ -33,4 +48,7 @@ while True:
 # save expenses to expenses.json file here
 # https://www.geeksforgeeks.org/read-write-and-parse-json-using-python/ (Writing JSON to a file in Python)
 pass
+
+with open("expenses.json", "w") as outfile:
+    json.dump(expenses, outfile)
 
